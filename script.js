88bc79deaaf57d42323d4d49d0bd3cc5626ed28f
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-05-26 16:56:12 +0530

    (fix) comment console logging, fix size of ball

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const divElem = document.querySelector(".row-2>div");
 const htmlElem = document.querySelector(":root");
 
 htmlElem.addEventListener("click", showHide);
-htmlElem.addEventListener("click", sayHi);
+// htmlElem.addEventListener("click", sayHi);
 document.addEventListener("keydown", showHide);
-document.addEventListener("keydown", sayHello);
+// document.addEventListener("keydown", sayHello);
 
 function showHide() {
   divElem.classList.toggle("showing");
