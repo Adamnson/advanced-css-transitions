dc1e119263d2e41387badb65a77b3262143d4f2b
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-05-26 17:00:38 +0530

    (feat) change navbar, add second page

---
+++ script1.js
@@ -0,0 +1,24 @@
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const down = document.getElementById("down");
+const rotate = document.getElementById("rotate");
+
+down.addEventListener("click", () => {
+  box1.classList.add("animate");
+  box1.addEventListener('animationend', removeAnimateClass)
+});
+
+let removeAnimateClass = () => {
+  box1.removeEventListener('animationend', removeAnimateClass);
+  box1.classList.remove('animate');
+}
+
+rotate.addEventListener("click", () => {
+  box2.classList.add("rotate-box");
+  box2.addEventListener('animationend', removeRotateClass);
+});
+
+let removeRotateClass = () => {
+  box2.removeEventListener('animationend', removeRotateClass);
+  box2.classList.remove('rotate-box');
+}
